a6a236a060612daae98efe7a0c222324e483427f
syaqub2 <s.yaqub2@wlv.ac.uk>
2026-06-19 22:43:58 +0100

    Update stripe.js

--- routes/stripe.js
+++ routes/stripe.js
@@ -6,6 +6,12 @@ const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_KEY
+ {
+    auth: {
+      autoRefreshToken: false,
+      persistSession: false
+    }
+  }
 );
 
 router.post('/webhook', async (req, res) => {
